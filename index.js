5d6fd847f32f1cf14faea5b7e8df09c753935766
Felix Kipkurui Langat <fexlangat@gmail.com>
2024-03-21 16:47:18 +0300

    modified index.js

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your code in this file!
+
+
+
+const currentUser = `Felix Langat`
+
+const welcomeMessage = `Welcome to Flatbook, ` + currentUser + `!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+
+
+
+
+
+
+
+  describe('shortGreeting', () => {
+    it(`contains "Welcome, "`, () => {
+      expect(shortGreeting).to.have.string('Welcome, ');
+    });
+
+    it("contains the first initial of the name stored in the 'currentUser' variable", () => {
+      const firstInitial = currentUser[0];
+      const restOfName = currentUser.slice(1);
+
+      expect(shortGreeting).to.have.string(firstInitial);
+      expect(shortGreeting).to.not.have.string(restOfName);
+    });
+
+    it('ends with an exclamation point', () => {
+      expect(shortGreeting.substr(-1)).to.eq('!');
+    });
+  });
